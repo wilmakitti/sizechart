d1b3c062282f7abf98bc9553f9ae5bb1b56127b5
Wilma Kitti <wilmakitti54@gmail.com>
2024-03-19 14:48:34 +0000

    Deleted a user managing file

---
+++ new_size_guide.py
@@ -0,0 +1,43 @@
+# Function to suggest the size for the user based on the input
+def size_suggestion():
+
+    
+    return size_suggestion
+
+bust_measurement = 0
+waist_measurement = 0
+hip_measurement = 0
+
+def body_measurements():
+    print("Please enter your measurements to get accurate size recommendations: ")
+bust_measurement = int(input("Bust: "))
+waist_measurement = int(input("Waist: "))
+hip_measurement = int(input("Hips: "))
+
+bust_measurement = float(input("Bust: "))
+waist_measurement = float(input("Waist: "))
+hip_measurement = float(input("Hips: "))
+print("Matching the inputs with the size chart...")
+
+top_average = (bust_measurement + waist_measurement) / 2
+bottom_average = (waist_measurement + hip_measurement) / 2
+dress_average = (waist_measurement + hip_measurement + bust_measurement) / 3
+
+print(f"Bust {bust_measurement}")
+print(f"Waist {waist_measurement}")
+print(f"Hips {hip_measurement}")
+
+
+
+
+
+
+
+
+def convert_cm_to_inches(self, value):
+    return value * 0.393701
+
+top_average = (bust_measurement + waist_measurement) / 2
+bottom_average = (waist_measurement + hip_measurement) / 2
+dress_average = (waist_measurement + hip_measurement + bust_measurement) / 3
+
